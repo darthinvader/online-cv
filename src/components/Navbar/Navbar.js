5fd44a96e9f42d7dc9107cdb423bc4727f80cb1b
darthinvader <psaradakisj@gmail.com>
2021-08-09 15:54:48 +0300

    [feat] navbar added links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,8 +4,29 @@ const Navbar = () => {
   return (
     <>
       <div className={styles.pageCentering}>
-        <nav className={styles.navbar}></nav>
+        <nav className={styles.navbar}>
+          <ul className={styles.navList}>
+            <li>
+              <a href="#About">About Me</a>
+            </li>
+            <li>
+              <a href="#Projects">Projects</a>
+            </li>
+            <li>
+              <a href="#CV">CV</a>
+            </li>
+            <li>
+              <a href="#Contact">Contact Me</a>
+            </li>
+          </ul>
+          <ul className={styles.navList}>
+            <li>
+              <a href="#Home"> Home</a>
+            </li>
+          </ul>
+        </nav>
       </div>
+      <div className={styles.middleCircle}></div>
     </>
   );
 };

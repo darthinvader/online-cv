3c7ccbf22840a024d6ce32d9282073818e08f01e
darthinvader <psaradakisj@gmail.com>
2021-08-11 10:31:47 +0300

    [feat] project template

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -5,6 +5,21 @@ const Project = ({
   title,
   subTitle,
   description,
-}) => {};
+}) => {
+  return (
+    <a href={link}>
+      <div className="project-container">
+        <div className="image-container">
+          <img src={imgLink} alt={title} />
+        </div>
+        <div className="project-content">
+          <h2 className="project-title">{title}</h2>
+          <h3 className="project-subtitle">{subTitle}</h3>
+          <p className="project-description">{description}</p>
+        </div>
+      </div>
+    </a>
+  );
+};
 
 export default Project;

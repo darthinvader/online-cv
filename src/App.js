a32f622fbedd3f7914c0ad4820cd764cea4d13af
darthinvader <psaradakisj@gmail.com>
2021-08-11 12:16:02 +0300

    [feat] page centering and container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
+import styles from "./App.module.scss";
 function App() {
-  return <Navbar />;
+  return (
+    <div className={styles.PageCentering}>
+      <div className={styles.Container}>
+        <Navbar />
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,29 +3,27 @@ import styles from "./Navbar.module.scss";
 const Navbar = () => {
   return (
     <>
-      <div className={styles.pageCentering}>
-        <nav className={styles.navbar}>
-          <ul className={`${styles.navList} + ${styles.listUpper}`}>
-            <li>
-              <a href="#About">About Me</a>
-            </li>
-            <li>
-              <a href="#Projects">Projects</a>
-            </li>
-            <li>
-              <a href="#CV">CV</a>
-            </li>
-            <li>
-              <a href="#Contact">Contact Me</a>
-            </li>
-          </ul>
-          <ul className={styles.navList}>
-            <li>
-              <a href="#Home"> Home</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className={styles.navbar}>
+        <ul className={`${styles.navList} + ${styles.listUpper}`}>
+          <li>
+            <a href="#About">About Me</a>
+          </li>
+          <li>
+            <a href="#Projects">Projects</a>
+          </li>
+          <li>
+            <a href="#CV">CV</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact Me</a>
+          </li>
+        </ul>
+        <ul className={styles.navList}>
+          <li>
+            <a href="#Home"> Home</a>
+          </li>
+        </ul>
+      </nav>
       <div className={styles.middleCircle}></div>
     </>
   );

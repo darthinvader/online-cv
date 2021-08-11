035f47e7244540ee47e47be1cceed72c93dadd6c
darthinvader <psaradakisj@gmail.com>
2021-08-11 10:39:51 +0300

    [feat] added css

--- src/components/Projects/ProjectContainer.js
+++ src/components/Projects/ProjectContainer.js
@@ -1,3 +1,7 @@
-const Projects = () => {};
+import styles from "ProjectContainer.module.scss";
+
+const Projects = () => {
+  return <div className={styles.ProjectContainer}>{/* Projects here */}</div>;
+};
 
 export default Projects;

250b090334091e408142be74521226e8675e7926
darthinvader <psaradakisj@gmail.com>
2021-08-12 16:59:26 +0300

    [feat] Footer completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import styles from "./App.module.scss";
 import ProjectsContainer from "./components/Projects/ProjectsContainer";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className={styles.PageCentering}>
       <div className={styles.Container}>
         <Navbar />
         <ProjectsContainer />
+        <Footer />
       </div>
     </div>
   );

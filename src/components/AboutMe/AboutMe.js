39634faeac2f4b5c17c9d43faaad53dd99c70392
darthinvader <psaradakisj@gmail.com>
2021-08-13 12:35:41 +0300

    [feat] About me experiences added

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,35 @@
+import TextTypperTitle from "../TextTypperTitle/TextTypperTitle";
+import Experience from "./Experience/Experience";
+
 const AboutMe = () => {
   return (
-    <div id="About">
-      <div className="Card"></div>
-      <div className="Card"></div>
-    </div>
+    <>
+      <TextTypperTitle title="About Me" />
+      <div id="About">
+        <div className="Card">
+          <h2>Experience</h2>
+          <Experience
+            ExperienceName="Tom Sawyer Software"
+            ExperienceDetail="Herakleion,Greece | May 2019- Oct 2019"
+            Description="Java Developer for the Perspectives Product"
+          />
+          <Experience
+            ExperienceName="Convert Group"
+            ExperienceDetail="Athens,Greece | April 2020 - Present"
+            Description="Python Developer for with emphasis on Web Scraping with the Scrapy
+            API."
+          />
+        </div>
+        <div className="Card">
+          <h2>Education</h2>
+          <Experience
+            ExperienceName="Computer Science"
+            ExperienceDetail="University Of Crete | Herakleion,Greece | April 2020 - Present"
+            Description=""
+          />
+        </div>
+      </div>
+    </>
   );
 };
 

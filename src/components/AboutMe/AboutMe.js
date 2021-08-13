3f3e08a48a148830a2d52c4aa645b95ea8da79ed
darthinvader <psaradakisj@gmail.com>
2021-08-13 17:21:01 +0300

    [feat] div id in correct location

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -4,33 +4,31 @@ import styles from "./AboutMe.module.scss";
 
 const AboutMe = () => {
   return (
-    <>
+    <div id="About">
       <TextTypperTitle title="About Me" />
-      <div id="About">
-        <div className={styles.Card}>
-          <h2 className={styles.Subtitle}>Experience</h2>
-          <Experience
-            ExperienceName="Tom Sawyer Software"
-            ExperienceDetail="Herakleion,Greece | May 2019- Oct 2019"
-            Description="Java Developer for the Perspectives Product"
-          />
-          <Experience
-            ExperienceName="Convert Group"
-            ExperienceDetail="Athens,Greece | April 2020 - Present"
-            Description="Python Developer for with emphasis on Web Scraping with the Scrapy
+      <div className={styles.Card}>
+        <h2 className={styles.Subtitle}>Experience</h2>
+        <Experience
+          ExperienceName="Tom Sawyer Software"
+          ExperienceDetail="Herakleion,Greece | May 2019- Oct 2019"
+          Description="Java Developer for the Perspectives Product"
+        />
+        <Experience
+          ExperienceName="Convert Group"
+          ExperienceDetail="Athens,Greece | April 2020 - Present"
+          Description="Python Developer for with emphasis on Web Scraping with the Scrapy
             API."
-          />
-        </div>
-        <div className={styles.Card}>
-          <h2 className={styles.Subtitle}>Education</h2>
-          <Experience
-            ExperienceName="Computer Science"
-            ExperienceDetail="University Of Crete | Herakleion,Greece | 2013 - 2018"
-            Description=""
-          />
-        </div>
+        />
       </div>
-    </>
+      <div className={styles.Card}>
+        <h2 className={styles.Subtitle}>Education</h2>
+        <Experience
+          ExperienceName="Computer Science"
+          ExperienceDetail="University Of Crete | Herakleion,Greece | 2013 - 2018"
+          Description=""
+        />
+      </div>
+    </div>
   );
 };
 

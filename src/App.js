ea26f4983f1db4983084936abcec75b6821f13a0
darthinvader <psaradakisj@gmail.com>
2021-08-13 12:24:13 +0300

    [feat] added About me to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar/Navbar";
 import styles from "./App.module.scss";
 import ProjectsContainer from "./components/Projects/ProjectsContainer";
 import Footer from "./components/Footer/Footer";
+import AboutMe from "./components/AboutMe/AboutMe";
 function App() {
   return (
     <div className={styles.PageCentering}>
       <div className={styles.Container}>
         <Navbar />
         <ProjectsContainer />
+        <AboutMe />
         <Footer />
       </div>
     </div>
---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+  return (
+    <div id="About">
+      <div className="Card"></div>
+      <div className="Card"></div>
+    </div>
+  );
+};
+
+export default AboutMe;

cc7b5eae57597f3ed2fd2adbe36628dc126d0b5e
darthinvader <psaradakisj@gmail.com>
2021-08-13 13:06:59 +0300

    [feat] Experience css style

--- src/components/AboutMe/Experience/Experience.js
+++ src/components/AboutMe/Experience/Experience.js
@@ -1,10 +1,12 @@
+import styles from "./Experience.module.scss";
+
 const Experience = ({ ExperienceName, ExperienceDetail, Description }) => {
   return (
     <>
-      <div className="Experience">
-        <h3 className="ExperienceName">{ExperienceName}</h3>
-        <h4 className="ExperienceDetails">{ExperienceDetail}</h4>
-        <p className="Description">{Description}</p>
+      <div className={styles.Experience}>
+        <h3 className={styles.Name}>{ExperienceName}</h3>
+        <h4 className={styles.Details}>{ExperienceDetail}</h4>
+        <p className={styles.Description}>{Description}</p>
       </div>
     </>
   );

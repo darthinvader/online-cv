0f6b1e7f82ac20f19697530a6c56a50616eadea0
darthinvader <psaradakisj@gmail.com>
2021-08-13 18:18:59 +0300

    [feat] added Home to App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import styles from "./App.module.scss";
 import ProjectsContainer from "./components/Projects/ProjectsContainer";
 import Footer from "./components/Footer/Footer";
 import AboutMe from "./components/AboutMe/AboutMe";
+import Home from "./components/Home/Home";
 function App() {
   return (
     <div className={styles.PageCentering}>
       <div className={styles.Container}>
         <Navbar />
+        <Home />
         <ProjectsContainer />
         <AboutMe />
         <Footer />

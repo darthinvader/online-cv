33608c91b5507f8ed78100d2d036e72264ff96ac
darthinvader <psaradakisj@gmail.com>
2021-08-13 12:35:32 +0300

    [feat] Experience No Css

--- src/components/AboutMe/Experience/Experience.js
+++ src/components/AboutMe/Experience/Experience.js
@@ -1,5 +1,13 @@
-const Experience = () => {
-  return <div></div>;
+const Experience = ({ ExperienceName, ExperienceDetail, Description }) => {
+  return (
+    <>
+      <div className="Experience">
+        <h3 className="ExperienceName">{ExperienceName}</h3>
+        <h4 className="ExperienceDetails">{ExperienceDetail}</h4>
+        <p className="Description">{Description}</p>
+      </div>
+    </>
+  );
 };
 
 export default Experience;

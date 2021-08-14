b28b5c013c5529d3353e42ac6c2ff383d7f2691b
darthinvader <psaradakisj@gmail.com>
2021-08-14 12:49:18 +0300

    [feat] Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,30 @@
 import styles from "./Home.module.scss";
+import TextTypper from "../TextTypper/TextTypper";
 
 const Home = () => {
-  return <div className={styles.Home}></div>;
+  return (
+    <div className={styles.Home}>
+      <img
+        src="https://i.imgur.com/yuBq01x.png"
+        alt="computer"
+        className={styles.Image}
+      ></img>
+      <div className={styles.Text}>
+        <h1 className={styles.Title}>
+          <TextTypper
+            text="Hello I am Yiannis Psaradakis"
+            timerMultiplier={80}
+          />
+        </h1>
+        <p className={styles.Details}>
+          <TextTypper
+            text="Front End Developer with Background in Python and Java"
+            timerMultiplier={70}
+          />
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

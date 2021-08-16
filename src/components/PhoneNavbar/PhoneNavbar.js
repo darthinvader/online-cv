a7b011c0e9ecae2df75496d73554542235a4756e
darthinvader <psaradakisj@gmail.com>
2021-08-16 12:21:14 +0300

    [feat] template for Phone Navbar

--- src/components/PhoneNavbar/PhoneNavbar.js
+++ src/components/PhoneNavbar/PhoneNavbar.js
@@ -1,3 +1,30 @@
-const PhoneNavbar = () => {};
+import styles from "./PhoneNavbar.module.scss";
+
+const PhoneNavbar = () => {
+  return (
+    <>
+      <div className={styles.Burger}></div>
+      <nav className={styles.navbar}>
+        <ul className={styles.navList}>
+          <li>
+            <a href="#Home"> Home</a>
+          </li>
+          <li>
+            <a href="#About">About Me</a>
+          </li>
+          <li>
+            <a href="#Projects">Projects</a>
+          </li>
+          <li>
+            <a href="#CV">CV</a>
+          </li>
+          <li>
+            <a href="#Contact">Contact Me</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default PhoneNavbar;

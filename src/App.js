98bcc647d1a644ffa5bf11050d26816e63b98cf4
darthinvader <psaradakisj@gmail.com>
2021-08-16 12:50:30 +0300

    [feat] add phone navbar to app

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import ProjectsContainer from "./components/Projects/ProjectsContainer";
 import Footer from "./components/Footer/Footer";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Home from "./components/Home/Home";
+import PhoneNavbar from "./components/PhoneNavbar/PhoneNavbar";
 function App() {
   return (
     <div className={styles.PageCentering}>
       <div className={styles.Container}>
         <Navbar />
+        <PhoneNavbar />
         <Home />
         <ProjectsContainer />
         <AboutMe />

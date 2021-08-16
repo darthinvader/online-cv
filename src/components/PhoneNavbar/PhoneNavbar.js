31304972c64867e825cde17cfac6a51c8d6569a3
darthinvader <psaradakisj@gmail.com>
2021-08-16 13:04:27 +0300

    [feat] completed phone navbar

--- src/components/PhoneNavbar/PhoneNavbar.js
+++ src/components/PhoneNavbar/PhoneNavbar.js
@@ -1,10 +1,25 @@
 import styles from "./PhoneNavbar.module.scss";
-
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 const PhoneNavbar = () => {
+  const [extendNavbar, setExtendNavbar] = useState(false);
+
+  const changeState = () => {
+    setExtendNavbar(!extendNavbar);
+  };
+
+  const navbarStyle = extendNavbar
+    ? `${styles.navbar} ${styles.extend}`
+    : styles.navbar;
+
   return (
     <>
-      <div className={styles.Burger}></div>
-      <nav className={styles.navbar}>
+      <div className={styles.BurgerContainer}>
+        <div className={styles.Burger} onClick={changeState}>
+          <GiHamburgerMenu />
+        </div>
+      </div>
+      <nav className={navbarStyle}>
         <ul className={styles.navList}>
           <li>
             <a href="#Home"> Home</a>
